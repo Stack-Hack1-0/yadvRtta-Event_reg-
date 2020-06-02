e9e7e358d55b98d4aac6372d164e7582601fd1af
Saiprava <rsaiprava99@gmail.com>
2020-06-02 22:20:28 +0530

    clientside

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <div>Let's start working..</div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+import React ,{ Component } from 'react';
+import RegForm from './Components/regForm/RegForm';
+import './App.css';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
+import Main from './Components/Main/Main';
+class App extends Component{
+  render(){
+    return(
+      <BrowserRouter>
+      <div className="App">
+      <Switch>
+        <Route exact path="/" component={Main}></Route>
+        <Route exact path="/register" component={RegForm}></Route>
+        </Switch>
+        </div>
+        </BrowserRouter>
+    );
+  }
 }
+
 
 export default App;
